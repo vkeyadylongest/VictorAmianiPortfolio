00d7a7ac134e9bc066aa6f4282c021779ce94317
Victor <vkeyadylongest@gmail.com>
2025-08-06 20:08:46 +0300

    javascript functionality

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+
+const typed = new Typed(".typed", {
+  strings: ["Front-End Developer", "JavaScript Enthusiast", "Problem Solver"],
+  typeSpeed: 60,
+  backSpeed: 40,
+  loop: true,
+});
